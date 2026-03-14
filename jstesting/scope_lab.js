1724e7261d940b122d8f90dc99f344bfadc1f909
delsDin <dels.git@gmail.com>
2026-03-13 21:05:24 -0400

    Monter la portee global

--- jstesting/scope_lab.js
+++ jstesting/scope_lab.js
@@ -3,9 +3,15 @@ var globalVar = "Je suis une variable globale";
 let globalLet = "Je suis aussi une variable globale";
 const globalConst = "Je suis une constante globale"
 
+
 {
     // Portée du bloc
     var blockVar = "Je suis une var avec portée de bloc";
     let blockLet = "Je suis un let avec portée de bloc";
     const blockConst = "Je suis une constante avec portée de bloc";
 }
+
+// Portée globale
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
