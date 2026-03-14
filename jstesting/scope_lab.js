1327696fe52b91cddc90042315bf015c5c53ac51
delsDin <dels.git@gmail.com>
2026-03-13 20:53:43 -0400

    Dclarer et initialiser des variables avec diffrentes portes

---
+++ jstesting/scope_lab.js
@@ -0,0 +1,11 @@
+// Portée globale
+var globalVar = "Je suis une variable globale";
+let globalLet = "Je suis aussi une variable globale";
+const globalConst = "Je suis une constante globale"
+
+{
+    // Portée du bloc
+    var blockVar = "Je suis une var avec portée de bloc";
+    let blockLet = "Je suis un let avec portée de bloc";
+    const blockConst = "Je suis une constante avec portée de bloc";
+}
